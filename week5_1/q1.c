0008fbb687f34ba8728e8d6e54020ef750c16197
Lin Yun Wen <l40303k@gmail.com>
2018-04-28 21:57:21 +0800

    Feature: add FuncI

--- week5_1/q1.c
+++ week5_1/q1.c
@@ -5,8 +5,21 @@ uint8_t Func32(uint32_t x) {
     return x ? Func32(x >> 1) - 1 : 32;
 }
 
+uint8_t FuncI(uint32_t x)
+{
+    if (!x) return 32;
+    int n = 1;
+    if (!(x >> 16)) { n += 16; x <<= 16; }
+    if (!(x >> 24)) { n +=  8; x <<=  8; }
+    if (!(x >> 28)) { n +=  4; x <<=  4; }
+    if (!(x >> 30)) { n +=  2; x <<=  2; }
+    n = n - (x >> 31);
+    return n;
+}
+
 int main() {
-    uint8_t result = Func32(0x0000ffff);
+    // uint8_t result = Func32(0x0000ffff);
+    uint8_t result = FuncI(0x00003fff);
     printf("result: %" PRIu8 "\n", result);
     return 0;
 }
